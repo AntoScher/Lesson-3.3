d97f4e2168cc9414d13a4c957807ced83270bd49
AntoScher <asc8er@gmail.com>
2024-10-11 16:04:43 +0300

    images

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pygame
-
+import random
 pygame.init()
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("C:\DEV\GitHupRepo\Lesson-3.3")
+icon = pygame.image.load("img/Tir.png")
+pygame.display.set_icon(icon)
+
+target_image=pygame.image.load("img/target.png")
+target_width = 50
+target_height = 50
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 running=True
 while running:
     pass
