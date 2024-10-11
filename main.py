ef2630bbec0bc3d0a8047786b7423335df2d6d89
AntoScher <asc8er@gmail.com>
2024-10-11 15:17:02 +0300

    add picture Tir

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
 
 pygame.init()
-
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("C:\DEV\GitHupRepo\Lesson-3.3")
 running=True
 while running:
     pass
