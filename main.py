0bd56580d511cfc16fa1ad21a661ecae6131a14d
AntoScher <asc8er@gmail.com>
2024-10-12 18:10:24 +0300

    add code & picture

--- main.py
+++ main.py
@@ -10,14 +10,27 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/Tir.png")
 pygame.display.set_icon(icon)
 
-target_image=pygame.image.load("img/target.png")
-target_width = 50
-target_height = 50
+target_image=pygame.image.load("img/target73X73.png")
+target_width = 73
+target_height = 75
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 running=True
 while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+
+
+    pygame.display.update()
     pass
 
 pygame.quit()
